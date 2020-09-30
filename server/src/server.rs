bde2db061ed4d3c2885a761b82400157a2b4eb94
Darjusch <Darjusch.schrand@code.berlin>
2020-09-30 09:36:35 +0200

    parsing request

--- server/src/server.rs
+++ server/src/server.rs
@@ -1,6 +1,7 @@
 use std::net::TcpListener;
-use std::io::Read;
+use std::io::{Read, Write};
 use std::convert::TryFrom;
+use std::convert::TryInto;
 use crate::http::Request;
 
 pub struct Server {
@@ -19,12 +20,24 @@ impl Server {
         loop {
           match listener.accept() {
             Ok((mut stream, _)) => {
+              // Gives a Hello World back to the user!
+              // match stream.write(b"HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!") {
+              //   Ok(_) => {
+              //     println!("Success");
+              //   }
+              //   Err(e) => {
+              //     println!("Error: {}", e);
+              //   }
+              // }
               // find a way to increase buffer size || how to add the buffer to read it then complete
               let mut buffer = [0; 1024];
               match stream.read(&mut buffer) {
                 Ok(_) => {
                   println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                  Request::try_from(&buffer[..]);
+                  match Request::try_from(&buffer[..]) {
+                    Ok(request) => {},
+                    Err(e) => println!("Failed to parse a request:  {}", e),
+                  }
                 },
                 Err(e) => println!("Failed to read from connection: {}", e),
               };
