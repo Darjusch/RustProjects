b2a692d42e7ffeb386b6c7920e081ca245a02a4d
Darjusch <Darjusch.schrand@code.berlin>
2020-10-01 13:40:44 +0200

    rounting

--- server/src/website_handler.rs
+++ server/src/website_handler.rs
@@ -1,10 +1,27 @@
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
+use super::http::{Method, Request, Response, StatusCode};
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+  public_path: String
+}
+
+impl WebsiteHandler {
+  pub fn new(public_path: String) -> Self {
+    Self { public_path }
+  }
+}
 
 impl Handler for WebsiteHandler {
   fn handle_request(&mut self, request: &Request) -> Response {
-    Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+    match request.method() {
+      Method::GET => match request.path() {
+        "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())),
+        "/admin/info" => Response::new(StatusCode::Ok, Some("<h1>Admin Info</h1>".to_string())),
+        "/admin/config" => Response::new(StatusCode::Ok, Some("<h1>Admin Config</h1>".to_string())),
+        "/admin/console" => Response::new(StatusCode::Ok, Some("<h1>Admin Console</h1>".to_string())),
+        _ => Response::new(StatusCode::NotFound, None),
+      }
+      _ => Response::new(StatusCode::NotFound, None),
+    }
   }
 }
