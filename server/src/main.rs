a41635929d2536f006093d7e1bdcad440b29d1ba
Darjusch <Darjusch.schrand@code.berlin>
2020-10-01 13:40:54 +0200

    environment variable

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,6 +4,7 @@ use http::Method;
 use http::Request;
 use server::Server;
 use website_handler::WebsiteHandler;
+use std::env;
 
 mod website_handler;
 mod server;
@@ -11,7 +12,10 @@ mod http;
 
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("publich path: {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
 
